ecee7f9f611259cd70d4387d37f759e11927f639
oseipoku <ospoku@gmail.com>
2023-07-13 19:21:35 +0000

    fixing errors

--- EKLEXIA/ViewComponents/AddMember.cs
+++ EKLEXIA/ViewComponents/AddMember.cs
@@ -24,14 +24,9 @@ namespace EKLEXIA.ViewComponents
                 MaritalStatuses = new SelectList(xct.MaritalStatuses.ToList(), nameof(MaritalStatus.Id), nameof(MaritalStatus.Name)),
                 Careers = new SelectList(xct.Careers.ToList(), nameof(Career.Id), nameof(Career.Name)),
                 Regions = new SelectList(xct.Regions.ToList(), nameof(Region.Id), nameof(Region.Name)),
-<<<<<<< HEAD
-
-                Groups = new SelectList(xct.Groups.ToList()),
-=======
-                Groups = new SelectList(xct.Groups.ToList(),nameof(Group.Id), nameof(Group.Name)),
+                
            
              
->>>>>>> 8a9db862277e6e9344738b61c746aff30cb31f45
                 Branches = new SelectList(xct.Branches.ToList(), nameof(Branch.Id), nameof(Branch.Name))
             };
 
